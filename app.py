993c8e6fbe1bdb921061f8462e7e463eab4e5076
peja016 <jerry79525@gmail.com>
2025-04-06 17:53:57 +0100

    updated add 404 page

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ load_dotenv() # Load environment variables from .env file
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/')
 def index():
     return render_template('index.html')
