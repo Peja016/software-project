7767325ecf9c497aa7abf9142b859024682e3555
peja016 <jerry79525@gmail.com>
2025-04-09 00:13:25 +0100

    updated getWeather

--- getWeatherData.py
+++ getWeatherData.py
@@ -9,7 +9,7 @@ url = 'https://api.openweathermap.org/data/2.5/weather'
 def getCurrentWeatherData(lat = 53.344, lon = -6.2672):
 
     # Construct the full URL (including API key and city parameter)
-    full_url = f'{url}?lat={lat}&lon={lon}&&appid={api_key}&units=metric'
+    full_url = f'{url}?lat={lat}&lon={lon}&appid={api_key}&units=metric'
 
     # Send a GET request
     response = requests.get(full_url)
