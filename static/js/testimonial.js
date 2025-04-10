35f46b14f490228d84dc30e98ff4ddbf3fd2713f
peja016 <jerry79525@gmail.com>
2025-04-10 23:02:36 +0100

    updated env

---
+++ static/js/testimonial.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const testimonials = document.querySelectorAll('.testimonial');
+    let currentIndex = 0;
+    
+    testimonials.forEach((testimonial, index) => {
+        if (index !== 0) {
+            testimonial.style.opacity = 0;
+            testimonial.style.visibility = 'hidden';
+        }
+    });
+
+    function showNextTestimonial() {
+        testimonials[currentIndex].style.opacity = 0;
+        testimonials[currentIndex].style.visibility = 'hidden';
+        
+        currentIndex = (currentIndex + 1) % testimonials.length;
+        
+        testimonials[currentIndex].style.opacity = 1;
+        testimonials[currentIndex].style.visibility = 'visible';
+    }
+
+    setInterval(showNextTestimonial, 6000);
+});
